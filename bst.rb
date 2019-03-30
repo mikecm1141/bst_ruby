5eb658c6e4fbe2236f7e251d1f88f027c511b9dd
Mike <mikecm@gmail.com>
2019-03-30 11:56:34 -0600

    Add search method

--- bst.rb
+++ bst.rb
@@ -5,6 +5,14 @@ class Bst
     @root = Node.new(key)
   end
 
+  def bst_print(node = @root)
+    unless node.nil?
+      bst_print(node.left)
+      puts node.key
+      bst_print(node.right)
+    end
+  end
+
   def insert(key, node = @root)
     node = Node.new(key) if node.nil?
 
@@ -14,6 +22,16 @@ class Bst
       node.right = insert(key, node.right)
     end
 
-    return node
+    node
+  end
+
+  def search(key, node = @root)
+    if key == node.key
+      node
+    elsif key < node.key
+      search(key, node.left)
+    elsif key > node.key
+      search(key, node.right)
+    end
   end
 end
